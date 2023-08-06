fbcc53f39510bbb12c5977ed8bdcb7623c5a4ce2
tossapong nuntanart <tosssapong.n@appman.co.th>
2023-08-06 13:39:31 +0700

    add new feature

--- discord-speaking-status.js
+++ discord-speaking-status.js
@@ -1,40 +1,25 @@
 listenForDiscordEvents = function (e) {
   let user = game.users.find(u=> u.flags["discord-speaking-status"]?.id == e.data.user_id)
-  if (!user) {
-    
-    let id = 'assign-' + e.data.user_id;
-    //console.log($(`#${id}`).length)
-    if ($(`#${id}`).length) {
-      if (e.data.evt=="SPEAKING_START") return $(`#${id}`).css({outline: '3px solid #3BA53B'});
-      if (e.data.evt=="SPEAKING_STOP") return $(`#${id}`).css({outline: 'unset'});
-    }
-    new Dialog({
-      title: e.data.user_id,
-      content: game.users.filter(u=> !u.flags["discord-speaking-status"]?.id).reduce((a,u)=>a+=`<option value="${u.id}">${u.name}</option>`,`<select name="user">`)+`</select>`,
-      buttons: {
-        confirm: {label:'confirm', callback: (html)=>{
-          game.users.get(html.find('select').val()).setFlag('discord-speaking-status', 'id', e.data.user_id)
-        }}
-      },
-      default: 'confirm'
-    },{id}).render(true)
-    return;
-  };
+  if (!user) return;
   Hooks.call('discordSpeakingEvent', e.data)
   let tokens = user.character?.getActiveTokens();
+
+  const isGMFreeSelection = game.settings.get("discord-speaking-status", "gmFreeSelect");
+  if(user.name =='Gamemaster' && isGMFreeSelection){
+    tokens = canvas.tokens.controlled;
+  }
+  
   if (e.data.evt=="SPEAKING_START") {
-    $(`#player-list > li[data-user-id="${user.id}"] span:first-child`).css({outline: '5px solid #3BA53B'});
     tokens.forEach(t => {
-      
-      $('#hud').append($(`<div class="speaking-token-marker ${t.id}" style="position: absolute; top: ${t.y}px; left: ${t.x}px; width: ${t.w}px; height: ${t.h}px; outline: ${canvas.grid.size/20}px solid #3BA53B; border-radius: ${canvas.grid.size/20}px;"></div>`));
-      $(`#token-action-bar li[data-token-id="${t.id}"]`).css({outline: '3px solid #3BA53B'});
+      $(`#player-list > li[data-user-id="${user.id}"] span:first-child`).css({outline: '5px solid #3BA53B'});
+      t.document.setFlag('discord-speaking-status', 'BorderColor', '3BA53B')
     });
+    
   }
   if (e.data.evt=="SPEAKING_STOP") {
     $(`#player-list > li[data-user-id="${user.id}"] span:first-child`).css({outline: 'unset'});
     tokens.forEach(t => { 
-      $('#hud').find(`div.speaking-token-marker.${t.id}`).remove(); 
-      $(`#token-action-bar li[data-token-id="${t.id}"]`).css({outline: 'unset'});
+      t.document.unsetFlag('discord-speaking-status', 'BorderColor')
     });
   }
 }
@@ -49,14 +34,54 @@ cleanDiscordSpeakingMarkers = function () {
   $(`#token-action-bar li`).css({outline: 'unset'});
 }
 
+
+colorizeBorder = function (token) {
+  
+  let borderColor = Color.from(token.document.getFlag('discord-speaking-status', 'BorderColor'))
+
+  if (!borderColor || isNaN(borderColor)) {
+    return;
+  }
+  
+  const thickness = 10; //border thickness
+  const sB = 1; //scale border
+  const nBS = canvas.dimensions.size / 100 //border grid scale (usually 1)
+  const p = 0 // border offfset negative for inside
+  const q = Math.round(p / 2)
+  const h = Math.round(thickness / 2);
+  const o = Math.round(h / 2);
+  const s = 1 // scale
+  const sW = sB ? (token.w - (token.w * s)) / 2 : 0
+  const sH = sB ? (token.h - (token.h * s)) / 2 : 0
+  token.border.lineStyle(h * nBS, borderColor.valueOf(), 1.0).drawRoundedRect(-o - q + sW, -o - q + sH, (token.w + h) * s + p, (token.h + h) * s + p, 3);
+} 
+
 Hooks.on('refreshToken', (t)=>{
+  colorizeBorder(t)
 	if (t.isPreview) return;
   $(`#hud > div.speaking-token-marker.${t.id}`).css({ top: `${t.y}px`, left: `${t.x}px`});
 });
 
+const unsecuredCopyToClipboard = (text) => { 
+  const textArea = document.createElement("textarea"); 
+  textArea.value=text; document.body.appendChild(textArea); 
+  textArea.focus();textArea.select(); 
+  try{
+    document.execCommand('copy')
+  }
+  catch(err){
+    console.error('Unable to copy to clipboard',err)
+  }
+  document.body.removeChild(textArea)
+};
+
 openDiscordWindow = async function () {
-  //let code = "const users = {};\nconst log = window.console.log.bind(window.console);\nwindow.console.log = (...args) => {\n  if (!args[1]) return log(...args);\n  if (typeof args[1] !== 'object') return log(...args);\n  let data = args[1].data;\n\tdata.evt = args[1].evt;\n\tif (data.evt == \"VOICE_STATE_UPDATE\") {\n\t\tusers[data.user.id] = `${data.user.username}#${data.user.discriminator}`\n\t\treturn console.log(users[data.user.id], 'added to users', users)\n\t}\n\tif (![\"SPEAKING_START\", \"SPEAKING_STOP\"].includes(data.evt)) return log(...args);\n\tdata.name = users[data.user_id];if (name == undefined) data.name = document.querySelector(`img[src*=\"${data.user_id}\"]`)?.parentElement?.querySelector(\"span\").innerHTML;\n\tdelete data.channel_id; delete data.user_id;\n\tlog('sending this data to window.opener', data);\n  window.opener.postMessage(data, '*');\n}"
-  //await window.navigator.clipboard.writeText(code);
+  let code = "const users = {};\r\nconst log = window.console.log.bind(window.console);\r\nwindow.console.log = (...args) => {\r\n  if (!args[1] || !window.opener)return log(...args);\r\n  if (typeof args[1] !== \'object\') return log(...args);\r\n  let data = args[1].data;\r\n\tdata.evt = args[1].evt;\r\n\tif (data.evt == \"VOICE_STATE_UPDATE\") {\r\n\t\tusers[data.user.id] = `${data.user.username}#${data.user.discriminator}`\r\n\t\treturn console.log(users[data.user.id], \'added to users\', users)\r\n\t}\r\n\tif (![\"SPEAKING_START\", \"SPEAKING_STOP\"].includes(data.evt)) return log(...args);\r\n\tdata.name = users[data.user_id];\r\n\tdata.nick = document.querySelector(`img[src*=\"${data.user_id}\"]`)?.parentElement?.querySelector(\"span\").innerHTML;\r\n\tlog(\'sending this data to window.opener\', data);\r\n  window.opener.postMessage(data, \'*\');\r\n}"
+  if (window.isSecureContext ) {
+    await window.navigator.clipboard.writeText(code);
+  } else {
+    unsecuredCopyToClipboard(code);
+  }
   channel = game.settings.get("discord-speaking-status", "channel");
   let parts = channel.split('/');
   window.open(`https://streamkit.discord.com/overlay/voice/${parts[4]}/${parts[5]}`)
@@ -70,6 +95,16 @@ Hooks.once("init", async () => {
     config: true,
     type: String,
     default: "",
+    requiresReload: false
+  });
+  
+  game.settings.register('discord-speaking-status', 'gmFreeSelect', {
+    name: `GM free selection`,
+    hint: `Game master can pick any token and it will brink when speak`,
+    scope: "world",
+    config: true,
+    type: Boolean,
+    default: false,
     requiresReload: false
   });
 
@@ -89,3 +124,13 @@ Hooks.on('renderUserConfig', (app, html, data)=>{
   `));
   html.find('input[name="flags.discord-speaking-status.id"]').val(data.user.flags["discord-speaking-status"]?.id)
 });
+
+Hooks.on('controlToken', (token,controlled)=>{
+  if(!controlled){
+    let borderColor = Color.from(token.document.getFlag('discord-speaking-status', 'BorderColor'))
+    if (!borderColor || isNaN(borderColor) ) {
+      return;
+    }
+    token.document.unsetFlag('discord-speaking-status', 'BorderColor')
+  }
+})
